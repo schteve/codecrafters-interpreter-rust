44c6b5b4c076bd9562bae4cddea5639df1974272
Steve Heindel <stevenheindel@gmail.com>
2024-08-31 16:34:16 -0400

    Parsing: Unary operators

--- src/parser.rs
+++ src/parser.rs
@@ -18,9 +18,15 @@ pub enum Literal {
     Nil,
 }
 
+pub enum Unary {
+    Negate(Box<Expr>),
+    Not(Box<Expr>),
+}
+
 pub enum Expr {
     Literal(Literal),
     Grouping(Box<Expr>),
+    Unary(Unary),
 }
 
 impl Expr {
@@ -32,11 +38,23 @@ impl Expr {
                 Literal::Bool(b) => print!("{b}"),
                 Literal::Nil => print!("nil"),
             },
-            Self::Grouping(grp) => {
+            Self::Grouping(expr) => {
                 print!("(group ");
-                grp.print();
+                expr.print();
                 print!(")");
             }
+            Self::Unary(un) => match un {
+                Unary::Negate(expr) => {
+                    print!("(- ");
+                    expr.print();
+                    print!(")");
+                }
+                Unary::Not(expr) => {
+                    print!("(! ");
+                    expr.print();
+                    print!(")");
+                }
+            },
         }
     }
 }
@@ -76,19 +94,49 @@ impl Parser {
     }
 
     fn parse_unary(&mut self) -> Result<Expr, ParseError> {
-        self.parse_primary()
+        self.peek()
+            .ok_or(ParseError::NoValidExpr)
+            .and_then(|t| match t.ttype {
+                TokenType::Minus => {
+                    self.advance();
+                    let expr = self.parse_unary()?;
+                    Ok(Expr::Unary(Unary::Negate(Box::new(expr))))
+                }
+                TokenType::Bang => {
+                    self.advance();
+                    let expr = self.parse_unary()?;
+                    Ok(Expr::Unary(Unary::Not(Box::new(expr))))
+                }
+                _ => self.parse_primary(),
+            })
     }
 
     fn parse_primary(&mut self) -> Result<Expr, ParseError> {
-        self.advance()
+        self.peek()
             .ok_or(ParseError::NoValidExpr)
             .and_then(|t| match t.ttype {
-                TokenType::Number(n) => Ok(Expr::Literal(Literal::Number(n))),
-                TokenType::String(s) => Ok(Expr::Literal(Literal::String(s))),
-                TokenType::True => Ok(Expr::Literal(Literal::Bool(true))),
-                TokenType::False => Ok(Expr::Literal(Literal::Bool(false))),
-                TokenType::Nil => Ok(Expr::Literal(Literal::Nil)),
+                TokenType::Number(n) => {
+                    self.advance();
+                    Ok(Expr::Literal(Literal::Number(n)))
+                }
+                TokenType::String(s) => {
+                    self.advance();
+                    Ok(Expr::Literal(Literal::String(s)))
+                }
+                TokenType::True => {
+                    self.advance();
+                    Ok(Expr::Literal(Literal::Bool(true)))
+                }
+                TokenType::False => {
+                    self.advance();
+                    Ok(Expr::Literal(Literal::Bool(false)))
+                }
+                TokenType::Nil => {
+                    self.advance();
+                    Ok(Expr::Literal(Literal::Nil))
+                }
                 TokenType::LeftParen => {
+                    self.advance();
                     let expr = self.parse_expr()?;
                     match self.advance() {
                         Some(Token {
